0333b421eddc93ce35f2102852d499245a02a487
Daniel Activa <daniel.sanchez@xarxatecactiva.com>
2026-06-26 11:00:11 +0200

    Initial Commit

---
+++ src/comprobación_dni.js
@@ -0,0 +1,6 @@
+const letras = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E', 'T'];
+let dni = "";
+
+do{
+
+}while(dni == false)
